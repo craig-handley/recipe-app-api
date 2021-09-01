cda2bb83aba74d17df862870ea5212ad6cce6903
Craig Handley <craig.handley@clicktravel.com>
2021-09-01 16:36:44 +0100

    hack to fix db issue

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -11,6 +11,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         """Handle the command"""
         self.stdout.write('Waiting for database...')
+        time.sleep(5)
         db_conn = None
         while not db_conn:
             try:
